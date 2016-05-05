cc988af72dbd00720112d23d2747466aa78cad67
Desirèe Oliverio <desireeoliverio@MacBook-Pro-di-admin.local>
2016-05-05 18:23:38 +0200

    fine responsive

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,25 @@
 $(document).ready(function() {
+
 	$('.header__menu').click(function() {
-		$(this).addClass('active');
+		 $(this).addClass('active');
 	})
-})
+		// selezione active del menu
+
+		// $('.j-menuToggle').on('click',function(){
+		// 	$('.header__container').toggleClass('menuMobile');
+		// return false;
+
+	$('.j-menuToggle').click(function(){
+
+		var selectedAccordion = $(this).parent().find('.header__navbar');
+
+		if ( selectedAccordion.is(':visible') ){  // TRUE
+			selectedAccordion.slideUp(300);
+
+		} else { //FALSE
+			$('.header__navbar').slideUp(300);
+			selectedAccordion.slideDown(300);
+		}
+
+ 	});
+ });
